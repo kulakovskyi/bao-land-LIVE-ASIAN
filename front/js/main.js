59c219850525d24ab1fa60014ac4f15861158f25
kulakovskyi <torchok9@gmail.com>
2022-09-20 15:52:19 +0300

    add script

--- front/js/main.js
+++ front/js/main.js
@@ -14,9 +14,25 @@ window.addEventListener('orientationchange', () => {
     // if (orientation.matches) {
     //     window.location.reload()
     // }
-})
+});
 
+(function () {
+    let url = new URL(window.location.href)
 
+    if (url.searchParams.has('qtag')) {
+        localStorage.setItem('qtag', url.searchParams.get('qtag'))
+    }
+
+    window.addEventListener('click', function (e) {
+        const parent = e.target.closest('a')
+        if (parent) {
+            e.preventDefault()
+            let link = new URL(parent.href)
+            link.searchParams.set('qtag', localStorage.getItem('qtag'))
+            document.location.href = link
+        }
+    })
+})();
 
 
 
